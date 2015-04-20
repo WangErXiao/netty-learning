b60ad849a39e8e7c89cb3ac6a937ad8f940e7d59
robin <863579016@qq.com>
2015-04-20 23:18:16 +0800

    ServerSocketChannel multi thread  accept

--- src/main/java/com/yao/nio/socketchannel/SocketChannelIsOpen.java
+++ src/main/java/com/yao/nio/socketchannel/SocketChannelIsOpen.java
@@ -1,35 +1,78 @@
 package com.yao.nio.socketchannel;
 
 import org.junit.Test;
+import org.omg.CORBA.TIMEOUT;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.nio.channels.Channel;
-import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
-import java.nio.channels.spi.SelectorProvider;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by yaozb on 15-4-20.
+ *
+
+ #!/bin/bash
+
+ i=100
+ while [ $i -ne 0 ]
+ do
+ telnet localhost 9999 | awk 'NR==4 {print $0}';
+ let i=($i-1);
+ done
+
+ *
+ *
+ *
+ *
+ *
  */
 public class SocketChannelIsOpen {
     @Test
-    public void testIsOpen() throws IOException {
-        ServerSocketChannel serverSocketChannel=ServerSocketChannel.open();
+    public void testIsOpen() throws IOException, InterruptedException {
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        //listening-------------
         serverSocketChannel.bind(new InetSocketAddress(9999));
-        Channel xx= SelectorProvider.provider().inheritedChannel();
-        System.out.println(xx);
-        if(serverSocketChannel!=null&&serverSocketChannel.isOpen()){
-            while (true){
-                SocketChannel channel=serverSocketChannel.accept();
-                Socket socket=channel.socket();
-                socket.getOutputStream().write("hello !!\n".getBytes());
-                socket.close();
-            }
-            //channel.configureBlocking(false);
+
+
+
+        ExecutorService executor= Executors.newFixedThreadPool(4);
+        executor.submit(new MyAcceptorTask(serverSocketChannel));
+        executor.submit(new MyAcceptorTask(serverSocketChannel));
+        executor.submit(new MyAcceptorTask(serverSocketChannel));
+        executor.submit(new MyAcceptorTask(serverSocketChannel));
+        executor.shutdown();
+        executor.awaitTermination(1, TimeUnit.HOURS);
 
         }
     }
-}
+
+    class MyAcceptorTask implements Runnable{
+        ServerSocketChannel serverSocketChannel;
+        public MyAcceptorTask(ServerSocketChannel serverSocketChannel) {
+            this.serverSocketChannel = serverSocketChannel;
+        }
+
+        @Override
+        public void run() {
+            if (serverSocketChannel != null && serverSocketChannel.isOpen()) {
+                while (true) {
+                    SocketChannel channel = null;
+                    try {
+                        channel = serverSocketChannel.accept();
+                        Socket socket = channel.socket();
+                        socket.getOutputStream().write(("hello !!"+ Thread.currentThread().getId()+"\n").getBytes());
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
